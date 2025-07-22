996c45dce8fe0c39394b88b99bb239db3681b8e0
Sasha <kluzodota@gmail.com>
2025-07-22 14:05:06 +0300

    solution

--- cinema/models.py
+++ cinema/models.py
@@ -6,6 +6,7 @@ from django.db import models
 from django.conf import settings
 from django.utils.text import slugify
 
+"""Main model"""
 
 class CinemaHall(models.Model):
     name = models.CharField(max_length=255)
